5a69cf7f8559f51843a54fa1afad21cbef7dd3f8
rivaldoSouzaS <rivaldosilva@si.fiponline.edu.br>
2022-09-20 14:36:01 -0300

    Configurando o Vue Router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav id="nav">
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Order</router-link>
+    </nav>
+</template>
+
+<script>
+    export default{
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
